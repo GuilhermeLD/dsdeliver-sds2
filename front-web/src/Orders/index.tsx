c4f53d65708966271b51771caea3ead69c9c11e0
Guilherme Lima <guilherme_limadavid@yahoo.com.br>
2021-01-06 17:09:07 -0300

    finish day2

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,27 @@
 import React from "react";
+import ProductList from "./ProductList";
 import StepsHeader from "./StepsHeader";
 import "./styles.css";
+import { useEffect, useState } from "react";
+import { Product } from "./types";
+import { fetchProducts } from "../api";
 
 function Orders() {
+    const [products, setProducts] = useState<Product[]>([]);
+      console.log(products);
+
+    useEffect(() => {
+        fetchProducts().then(response => setProducts(response.data)).catch(error => console.log(error))
+
+    },
+    
+    []);
+
   return (
-    <div className="Orders-container">
+    <div className="orders-container">
       <StepsHeader />
+      <ProductList products={products} />
+
     </div>
   );
 }
