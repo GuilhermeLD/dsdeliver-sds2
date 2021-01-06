fdcd98c754fdb30bdb0e3e18b6a8651828060e42
Guilherme Lima <guilherme_limadavid@yahoo.com.br>
2021-01-06 15:09:34 -0300

    Add Footer

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,30 +1,35 @@
-import './styles.css';
-import {ReactComponent as MainImage} from './main.svg'; 
+import "./styles.css";
+import { ReactComponent as MainImage } from "./main.svg";
+import React from "react";
+import Footer from "../footer";
 
 function Home() {
   return (
-    <div className="home-container">
-      <div className="home-content">
-        <div className="home-actions">
-          <h1 className="home-title">
-            faça seu pedido
-            <br />
-            que entregamos <br />
-            pra você!!!
-          </h1>
-          <h3 className="home-subtitle">    
-            Escolha o seu pedido e em poucos minutos <br /> levaremoss na sua
-            porta
-          </h3>
-            <a href="orders" className="home-btn-order">FAZER PEDIDO</a>
-
+    <>
+      <div className="home-container">
+        <div className="home-content">
+          <div className="home-actions">
+            <h1 className="home-title">
+              faça seu pedido
+              <br />
+              que entregamos <br />
+              pra você!!!
+            </h1>
+            <h3 className="home-subtitle">
+              Escolha o seu pedido e em poucos minutos <br /> levaremoss na sua
+              porta
+            </h3>
+            <a href="orders" className="home-btn-order">
+              FAZER PEDIDO
+            </a>
+          </div>
+          <div className="home-image">
+            <MainImage />
+          </div>
         </div>
-        <div className="home-image">
-                <MainImage />
-        </div>
-    
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ front-web/src/footer/index.tsx
@@ -0,0 +1,28 @@
+import "./styles.css";
+import { ReactComponent as YoutubeIcon } from "./youtube.svg";
+import { ReactComponent as LinkedinIcon } from "./linkedin.svg";
+import { ReactComponent as InstagramIcon } from "./instagram.svg";
+
+function Footer() {
+  return (
+    <footer className="main-footer">
+      App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+      <div className="footer-icons">
+        <a href="https://youtube.com/c/DevSuperior" target="_new">
+          
+          <YoutubeIcon />
+        </a>
+        <a href="https://linkedin.com/school/devsuperior" target="_new">
+         
+          <LinkedinIcon />
+        </a>
+        <a href="https://instagram.com/DevSuperior" target="_new">
+         
+          <InstagramIcon />
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
