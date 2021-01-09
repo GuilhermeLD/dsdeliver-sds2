cd399939177db209dfe4ee200168fbbd16d4695d
Guilherme Lima <guilherme_limadavid@yahoo.com.br>
2021-01-09 15:23:48 -0300

    Finish front-web

--- front-web/src/Orders/types.ts
+++ front-web/src/Orders/types.ts
@@ -7,9 +7,18 @@ export type Product = {
     imageUri:string;
 }
 
-export type OrderLocationdata = {
+export type OrderLocationData = {
     
         latitude: number;
         longitude: number;
         address: string;
 }
+
+type ProductID = {
+    id: number;
+}
+
+export type OrderPayload = {
+    products: ProductID[]; 
+} & OrderLocationData;
+
