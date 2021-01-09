d78c141595e66bb469d6d00255a13852fb18bdd4
Guilherme Lima <guilherme_limadavid@yahoo.com.br>
2021-01-09 12:58:41 -0300

    Search order Location

--- front-web/src/Orders/OrderLocation.tsx
+++ front-web/src/Orders/OrderLocation.tsx
@@ -1,56 +1,61 @@
 import { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import AsyncSelect from 'react-select/async';
+import AsyncSelect from "react-select/async";
 import { fetchLocalMapBox } from "../api";
+import { OrderLocationdata } from "./types";
 
 const initialPosition = {
-    lat:  -23.7279538,
-    lng:  -46.7159391
-}
+  lat: -23.7279538,
+  lng: -46.7159391,
+};
 
 type Place = {
-    label?:string;
-    value?:string;
-    position:{
-        lat:number
-        lng:number
-    };
+  label?: string;
+  value?: string;
+  position: {
+    lat: number;
+    lng: number;
+  };
+};
+
+type Props = {
+  onChangeLocation: (location: OrderLocationdata) => void;  
 }
 
-function OrderLocation() {
-    const [address, setAddress] = useState<Place>({
-        position: initialPosition
+function OrderLocation({ onChangeLocation }: Props) {
+  const [address, setAddress] = useState<Place>({
+    position: initialPosition,
+  });
 
-    })
+  const loadOptions = async (
+    inputValue: string,
+    callback: (places: Place[]) => void
+  ) => {
+    const response = await fetchLocalMapBox(inputValue);
 
-    const loadOptions = async (inputValue: string, callback: (places: Place[]) => void) => {
-        const response = await fetchLocalMapBox(inputValue);
-      
-        const places = response.data.features.map((item: any) => {
-          return ({
-            label: item.place_name,
-            value: item.place_name,
-            position: {
-              lat: item.center[1],
-              lng: item.center[0]
-            },
-            place: item.place_name,
-          });
-        });
-      
-        callback(places);
+    const places = response.data.features.map((item: any) => {
+      return {
+        label: item.place_name,
+        value: item.place_name,
+        position: {
+          lat: item.center[1],
+          lng: item.center[0],
+        },
+        place: item.place_name,
       };
-      
-      const handleChangeSelect = (place: Place) => {
-        setAddress(place);
-        //onChangeLocation({//
-        //  latitude: place.position.lat,//
-        //  longitude: place.position.lng,//
-         // address: place.label!//
-       //});//
-      };
+    });
 
+    callback(places);
+  };
 
+  const handleChangeSelect = (place: Place) => {
+    setAddress(place);
+    onChangeLocation({
+      latitude: place.position.lat,
+      longitude: place.position.lng,
+     address: place.label!
+    });
+  };
 
   return (
     <div className="order-location-container">
@@ -59,24 +64,29 @@ function OrderLocation() {
           Selecione onde o pedido deve ser entregue
         </h3>
         <div className="filter-container">
-            <AsyncSelect 
+          <AsyncSelect
             placeholder="Digite um endereço para entregar o pedido"
             className="filter"
             loadOptions={loadOptions}
-            onChange={value => handleChangeSelect(value as Place)}
-            />
+            onChange={(value) => handleChangeSelect(value as Place)}
+          />
         </div>
-        <MapContainer center={address.position} zoom={13} scrollWheelZoom>
-            <TileLayer
-              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={address.position}>
-              <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-              </Popup>
-            </Marker>
-          </MapContainer>
+        <MapContainer
+          center={address.position}
+          zoom={15}
+          key={address.position.lat}  
+          scrollWheelZoom
+        >
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={address.position}>
+            <Popup>
+              {address.label}
+            </Popup>
+          </Marker>
+        </MapContainer>
       </div>
     </div>
   );
--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -3,13 +3,15 @@ import ProductList from "./ProductList";
 import StepsHeader from "./StepsHeader";
 import "./styles.css";
 import { useEffect, useState } from "react";
-import { Product } from "./types";
+import { OrderLocationdata, Product } from "./types";
 import { fetchProducts } from "../api";
 import OrderLocation from "./OrderLocation";
 
 function Orders() {
     const [products, setProducts] = useState<Product[]>([]);
       console.log(products);
+
+    const [orderLocation, setOrderLocation] = useState<OrderLocationdata>();
 
     useEffect(() => {
         fetchProducts().then(response => setProducts(response.data)).catch(error => console.log(error))
@@ -22,7 +24,7 @@ function Orders() {
     <div className="orders-container">
       <StepsHeader />
       <ProductList products={products} />
-      <OrderLocation />
+      <OrderLocation onChangeLocation={location => setOrderLocation(location)}/>
 
     </div>
   );
